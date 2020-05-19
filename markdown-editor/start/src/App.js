37ff5d42d20716997c0c395538e8dcc63a34b7fa
Rich McMann <richmcmann@Richards-MacBook-Pro.local>
2020-05-19 15:29:57 -0600

    finished markdown app

--- markdown-editor/start/src/App.js
+++ markdown-editor/start/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import marked from "marked";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
 export default function App() {
+  const [markdown, setMarkdown] = useState("# sup");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea onChange={handleChange} value={markdown} />
 
-      <div className="preview" />
+      {/* <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      /> */}
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
