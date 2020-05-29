1f998ce2f17d9c730639b14ea0a9e55484fc7738
Rich McMann <richmcmann@Richards-MacBook-Pro.local>
2020-05-29 10:41:45 -0600

    final  phase of tab app

--- browser-tabs/start/src/App.js
+++ browser-tabs/start/src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from "react-router-dom";
-import Tab from "./components/Tab";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Features from "./pages/Features";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Routes from "./Routes";
 import "./App.css";
 
 function App() {
@@ -16,36 +9,10 @@ function App() {
     <Router>
       <div className="app">
         <div className="browser">
-          <div className="tabs">
-            <Tab>
-              <NavLink to="/" activeClassName="is-active" exact={true}>
-                Home
-              </NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/about" activeClassName="is-active">
-                About
-              </NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/features" activeClassName="is-active">
-                Features
-              </NavLink>
-            </Tab>
-          </div>
+          <Header />
 
           <div className="viewport">
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/features">
-                <Features />
-              </Route>
-              <Route path="/" exact={true}>
-                <Home />
-              </Route>
-            </Switch>
+            <Routes />
           </div>
         </div>
       </div>
